1b3dd7637d69578626f139805480ebc167e5590c
Fernando Taricano <Fernando.taricano@cpzseg.com.br>
2026-05-08 14:37:11 -0300

    chore: Edit client.ts

--- src/graph/client.ts
+++ src/graph/client.ts
@@ -334,7 +334,7 @@ export class GraphClient {
     const fileSize = stats.size;
 
     // Create upload session
-    const sessionUrl = endpoint + "/createUploadSession";
+    const sessionUrl = endpoint.replace(/\/content$/, "") + "/createUploadSession";
     const sessionData = {
       item: {
         "@microsoft.graph.conflictBehavior":
